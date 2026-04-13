9e3b48d3e3a973f75e3bee158b3c3a11b2b5efef
Diogo Harada <diogohharada@gmail.com>
2026-04-13 19:24:01 -0300

    Inicio do exercício 4

---
+++ Abstract_04/src/abstract_04/Conta.java
@@ -0,0 +1,9 @@
+package abstract_04;
+
+abstract class Conta {
+    
+    float saldo;
+    
+    abstract void depositar();
+    abstract void sacar();
+}
---
+++ Abstract_04/src/abstract_04/ContaCorrente.java
@@ -0,0 +1,15 @@
+package abstract_04;
+
+public class ContaCorrente extends Conta{
+
+    @Override
+    void depositar() {
+        
+    }
+
+    @Override
+    void sacar() {
+
+    }
+    
+}
